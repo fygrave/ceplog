7409b14e4828bb320cfc4f7cc3b4ba6e34954e00
Fyodor Y <fygrave@o0o.nu>
2012-05-20 08:39:18 +0800

    pom specs and code

--- src/main/java/nu/o0o/ceplog/CepLog.java
+++ src/main/java/nu/o0o/ceplog/CepLog.java
@@ -1,9 +1,15 @@
 package nu.o0o.ceplog;
+import nu.o0o.ceplog.processor.HttpAdapter;;
 
 public class CepLog
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "CepLog Started" );
+
+
+    public static void main(String[] args) throws Exception {
+        HttpAdapter proc = new HttpAdapter();
+        proc.run();
+        while (true) {
+        	
+        }
     }
 }
